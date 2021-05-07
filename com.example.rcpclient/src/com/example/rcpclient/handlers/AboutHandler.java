e653d5453b5be80ae920138f5d62d60f5ad38eb1
Lars Vogel <Lars.Vogel@vogella.com>
2021-05-07 12:02:14 +0200

    Update of example, includes setting the SAAJMetaFactory
    		System.setProperty("jakarta.xml.soap.SAAJMetaFactory",
    "com.sun.xml.messaging.saaj.soap.SAAJMetaFactoryImpl");

--- com.example.rcpclient/src/com/example/rcpclient/handlers/AboutHandler.java
+++ com.example.rcpclient/src/com/example/rcpclient/handlers/AboutHandler.java
@@ -8,6 +8,7 @@ import jakarta.xml.ws.Endpoint;
 public class AboutHandler {
 	@Execute
 	public void execute(Shell shell) {
+		System.setProperty("jakarta.xml.soap.SAAJMetaFactory", "com.sun.xml.messaging.saaj.soap.SAAJMetaFactoryImpl");
 		String url = "http://localhost:4434/miniwebservice";
 		Endpoint.publish(url, new WebServiceExample());
 	}
